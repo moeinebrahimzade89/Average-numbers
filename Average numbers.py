5c18436dedee98c85ab9872d601aa212c8969397
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-11 22:52:17 +0330

    Update Average numbers.py

--- Average numbers.py
+++ Average numbers.py
@@ -1,10 +1,11 @@
 Plural = 0
-Number = 0 
+Number = 0
 
 while True:
-    a = int(input("Number:"))
+    a = int(input("Number: "))
     Plural += a
-    if a == 0 : break
+    if a == 0:
+        break
     Number += 1
-    
-print(Plural/Number)
+
+print(Plural / Number)
